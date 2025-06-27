d845739424f05bbefb93cbbd34aee4cba5dbbb31
Heriberto Gonzalez <heriberto777@gmail.com>
2025-06-27 10:58:07 -0400

    roles change it to role

--- server/models/roleModel.js
+++ server/models/roleModel.js
@@ -148,7 +148,7 @@ roleSchema.pre("save", function (next) {
 roleSchema.virtual("userCount", {
   ref: "Usuario",
   localField: "_id",
-  foreignField: "roles",
+  foreignField: "role",
   count: true,
 });
 
